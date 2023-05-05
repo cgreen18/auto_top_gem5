22283022fcc78fef9aa51e4944403e2be22566a4
cgreen18 <green456@mnemosyne.ecn.purdue.edu>
2023-05-05 13:30:18 -0400

    cleaning up. slurm copied to local desktop

--- slurm/run_scripts/run_synth_noci.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-
-TLIM="10-00:00:00"
-
-sbatch -t $TLIM slurm/job_scripts/synth_noci_naive_no_lb
-
-# sbatch -t $TLIM slurm/job_scripts/synth_noci_naive_hops_lb
--- slurm/run_scripts/test_new_gem5_noci_100m.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-TLIM="20-00:00:00"
-sbatch --exclude=mnemosyne -t $TLIM slurm/job_scripts/parsec_noci_largemem_18GHz_500kB/100m/blackscholes_kite_large_noci
